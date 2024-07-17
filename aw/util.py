d8251275754309890e3fe5972dd22f90e246c29f
Haku <muffitoftea@outlook.cz>
2024-07-17 12:59:22 +0200

    Add load_queries() function.

--- aw/util.py
+++ aw/util.py
@@ -1,9 +1,13 @@
 import json
 import os
 
+from aw.constraint import Constraint
 from aw.error import CloseThreadError
-from aw import ROOT_DIR, CONFIG_FILE
+from aw.query import Query
 
+from aw import ROOT_DIR, CONFIG_FILE, QUERIES_FILE
+
+# maybe change name to load_config
 def config_loader(key_list: list[str]) -> dict[str]:
     """
     Loads configuration values from a JSON file for the specified keys.
@@ -26,10 +30,10 @@ def config_loader(key_list: list[str]) -> dict[str]:
     json_data = None
 
     try:
-        with open(os.path.join(ROOT_DIR, CONFIG_FILE), "r") as FILE:
+        with open(os.path.join(ROOT_DIR, CONFIG_FILE), "r", encoding="utf-8") as FILE:
             json_data = json.load(FILE)
     except json.JSONDecodeError as e:
-            raise CloseThreadError(f"JSON parsed unsuccessfully: {e}") 
+        raise CloseThreadError(f"JSON parsed unsuccessfully: {e}") 
     except IOError as e:
         raise CloseThreadError(f"Problem reading file: {e}")
 
@@ -38,3 +42,31 @@ def config_loader(key_list: list[str]) -> dict[str]:
         config_dict.update({key: value})
 
     return config_dict
+
+def load_queries() -> list[Query]:
+    with open(os.path.join(ROOT_DIR, QUERIES_FILE), "r", encoding="utf-8") as FILE:
+        data = json.load(FILE)
+
+    query_list = []
+
+    for query_data in data.get("queries"):
+        constraint_list = []
+
+        for constraint_data in query_data.get("constraint_list"):
+            constraint = Constraint(
+                key = constraint_data.get("key"),
+                value = constraint_data.get("value"),
+                relation = constraint_data.get("relation"),
+                asciize = constraint_data.get("asciize")
+            )
+            constraint_list.append(constraint)
+                    
+        query = Query(
+            query_string = query_data.get("query_string"),
+            constraint_list = constraint_list,
+            asciize = query_data.get("asciize")
+        )
+
+        query_list.append(query)
+
+    return query_list
